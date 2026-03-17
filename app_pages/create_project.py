040c8182a38e84e8025555fa8d0dc17446947e65
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:13:14 -0300

    Add create_project.py for project creation page

---
+++ app_pages/create_project.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("Create Project")
+    st.caption("Create a new carbon project workspace.")
+    st.info("This page will allow creation of a new project with basic metadata and target methodology.")
