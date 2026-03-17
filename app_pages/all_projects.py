b65398a1f5874c7f9a2d14785b7fc4950f9c792f
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:12:48 -0300

    Add all_projects.py for project management page

---
+++ app_pages/all_projects.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("All Projects")
+    st.caption("List and manage all projects.")
+    st.info("This page will show all saved projects and their current status.")
