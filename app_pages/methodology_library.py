4940d6fb410ee16119726d1c7a65de92ac686e3b
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:16:40 -0300

    Add methodology library page with basic layout

---
+++ app_pages/methodology_library.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("Methodology Library")
+    st.caption("Browse standards, methodologies, and structured requirement frameworks.")
+    st.info("This page will organize the methodology knowledge base.")
