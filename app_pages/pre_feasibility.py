e21769bf83c54b0101a30398bbbbfcf8426acd0d
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:13:46 -0300

    Add pre-feasibility page with initial rendering

---
+++ app_pages/pre_feasibility.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("Pre-Feasibility")
+    st.caption("Early screening for eligibility, methodology fit, and major blockers.")
+    st.info("This mode will perform an initial project screening.")
