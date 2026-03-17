138c1c9e3dadbbf5014b8fb1f2a043d0d6ec6275
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:16:18 -0300

    Add initial spreadsheet filler page with Streamlit

---
+++ app_pages/spreadsheet_filler.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("Spreadsheet Filler")
+    st.caption("Complete spreadsheet templates using project-specific data.")
+    st.info("This tool will help fill checklists, spreadsheets, and tabular templates.")
