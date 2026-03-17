07860dda7079b47e5b679a51d4a60f630125ae77
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:17:57 -0300

    Add user access page with basic UI elements

---
+++ app_pages/user_access_page.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("User Access")
+    st.caption("Manage users, roles, and access policies.")
+    st.info("This page will support user access review and management.")
