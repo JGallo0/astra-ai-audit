c201cf8962adcf104bb8e62ae1ac616d50128129
JGallo0 <barrosgallo@gmail.com>
2026-03-17 11:17:01 -0300

    Add settings page with title and description

---
+++ app_pages/settings_page.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render():
+    st.title("Settings")
+    st.caption("System and workspace settings.")
+    st.info("This page will contain product settings, preferences, and configuration options.")
