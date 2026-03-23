69f23e97156bbc711945f7435690da3d255d4f5d
JGallo0 <barrosgallo@gmail.com>
2026-03-23 11:32:07 -0300

    Update test_engine.py

--- scripts/test_engine.py
+++ scripts/test_engine.py
@@ -1,5 +1,14 @@
 from versioning.methodology_manager import get_requirements
 from engine.requirement_logic import run_engine
+from schemas.project_schema import get_demo_project_data
+
+project_data = get_demo_project_data()
+
+requirements = get_requirements()
+results = run_engine(project_data, requirements)
+
+for r in results:
+    print(f'{r["id"]} | {r["status"]} | {r["name"]}') run_engine
 
 project_data = {
     "eligibility": {
